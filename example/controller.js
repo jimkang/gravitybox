4d43a4716ff1e786797025e4c48c1c7b44ed3a00
Jim Kang <jimkang@gmail.com>
2015-03-02 14:27:33 -0500

    Alternate version where text bounces against other text.

--- example/controller.js
+++ example/controller.js
@@ -1,41 +1,140 @@
 ((function createController() {
+  var samples = [
+    '   // Begin the counter of the passed time',
+    ' // label2',
+    ' /// Collection of "assassin targets" (network adapters).',
+    'function(req, res){});',
+    '//   TODO to main',
+    ' // per gestire il cambio di ruolo',
+    'function_exists(\'drawAdsPlace\') ) drawAdsPlace(array(\'id\' => 1)',
+    '-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+    ' // Decompose statement',
+    '  // A fix for LDAP users. array_merge loses keys...',
+    ' //scanning can take ages',
+    ' //TODO: DI for cookies and OC_Config',
+    'function(dir) {',
+    '  // for primary groups',
+    'function composeLdapFilter($filterType) {',
+    'function prepareFilterForUsersInPrimaryGroup($groupDN, $search = \'\') {',
+    'function countUsersInGroup($gid, $search = \'\') {',
+    '  // only append colon if there are more entries',
+    'function setUp(){',
+    'function getMimetype($id) {',
+    'function basicOperation($operation, $path, $hooks = array(), $extraParam',
+    'function update($path, $time = null) {',
+    ' //make sure we have an new empty folder to work in',
+    ' // \\x y. y (y x) : N -> (N -> N) -> N',
+    ' /// </param>',
+    '  //TODO make this cleaner and wrap enqueue in a function',
+    ' // When the Fireplace adapter posts to the API it is generating a',
+    ' +//    mLocationClient = new LocationClient(this, this, this);',
+    'fns = xmalloc_array(unsigned int*, size*sizeof(unsigned int));',
+    'unction afterInstallation($keyOfInstalledExtension) {',
+    '-// Login account FORM',
+    '+//  MOASwitch.m',
+    ' // TODO ⚪️ に対して 0.f / 1.f をセット',
+    '  // Prepare necessary data for _cli_lowlevel user creation',
+    '+//  Copyright (c) 2015年 MOAI. All rights reserved.',
+    ' // Owned by the Channel object',
+    ' /// Only first invocation has effect.',
+    ' /// </summary>',
+    ' // do next state, return false if all done',
+    'func,    call_destroy_func,    sizeof(int),',
+    'fneq ($(NO_SECURE),true)',
+    'function<void(grpc::Status, ResponseType *)> on_done):',
+    'function<void(grpc::Status, ResponseType *)> on_done)',
+    'fn(self))',
+    ' // Client recv status',
+    ' // Will add threads when ready to execute',
+    'function<void(grpc::Status, ResponseType *)> on_done)',
+    'var excerptStream = createAnalysisToExcerptStream({',
+    'var analysisStream = createAnalysisStream();',
+    'createSampleCommitStream({}, function connectPipe(error, sampleCommitStream) {',
+    '    sampleCommitStream.pipe(analysisStream);',
+    '    analysisStream.pipe(excerptStream);',
+    '    done(null, excerptStream);'
+  ];
+
+  var colors = [
+    'hsla(0, 100%, 50%, 1.0)',
+    'hsla(18, 100%, 62%, 1.0)',
+    'hsla(48, 100%, 66%, 1)',
+    'hsla(123, 59%, 50%, 1)',
+    'hsla(219, 90%, 52%, 0.8)',
+    'hsla(278, 85%, 55%, 1)',
+    'hsla(305, 82%, 69%, 1)',
+    'white',
+    'hsl(24, 67%, 43%)',
+    'hsl(353, 29%, 41%)',
+    'hsl(146, 100%, 30%)',
+    'hsl(166, 100%, 30%)',
+    'hsl(63, 85%, 41%)',
+    'hsl(212, 100%, 36%)',
+    'hsl(198, 72%, 68%)',
+    'hsl(193, 100%, 37%)',
+    'hsl(312, 100%, 35%)',
+    'hsl(300, 74%, 39%)',
+    'hsl(333, 84%, 62%)',
+    'hsl(328, 100%, 41%)',
+    'hsl(353, 72%, 56%)',
+    'hsl(355, 85%, 54%)',
+    'hsl(346, 100%, 42%)',
+    'hsl(38, 100%, 50%)',
+    'hsl(15, 100%, 61%)',
+    'hsl(12, 100%, 77%)',
+    'hsl(45, 94%, 72%)',
+    'hsl(51, 89%, 60%)'
+  ];
+
   var gravitybox = createGravityBox({
     root: d3.select('#ball-layer'),
-    width: 400,
-    height: 500,
-    r: 20,
-    nodeClass: 'node',
-    nodeElementName: 'circle',
-    xAttr: 'cx',
-    yAttr: 'cy'
+    width: 1280,
+    height: 960,
+    r: 25,
+    nodeClass: 'tagbox',
+    nodeElementName: 'text',
+    xAttr: 'x',
+    yAttr: 'y',
+    processSelection: function setText(d) {
+      if (d.attrs && d.attrs.codeText) {
+        d3.select(this).text(d.attrs.codeText);        
+      }
+    },
+    rectRecoilFactor: 3.0
   });
 
-  var fill = d3.scale.category20();
 
-  function makeNode(i) { 
-    var color = fill(~~(Math.random() * 20));
+  function makeNode(codeText) { 
+    var color = colors[~~(Math.random() * colors.length)];
+
     return {
-      x: ~~(Math.random() * 400),
+      x: ~~(Math.random() * 1180),
       y: 0,
+      width: codeText.length * 3,
+      height: 15,
       attrs: {
-        r: 20
+        width: codeText.length * 3,
+        height: 15,
+        codeText: codeText,
+        transform: 'rotate(' + ~~(Math.random() * 360) + ')'
       },
       styles: {
-        fill: color,
-        stroke: d3.rgb(color).darker()
+        fill: color
       }
     };
   }
-  var nodes = d3.range(0).map(makeNode);
+
+  var nodes = samples.slice(0, samples.length/10).map(makeNode);
   gravitybox.add(nodes);
   gravitybox.render();
 
   var wavesUnleashed = 0;
   var intervalKey = setInterval(function unleashMore() {
-    if (wavesUnleashed > 1) {
+    if (wavesUnleashed > 9) {
       clearInterval(intervalKey);
     }
-    var nextWave = d3.range(50).map(makeNode);
+    var nextWave = samples.slice(samples.length/10 * wavesUnleashed)
+      .map(makeNode);
     gravitybox.add(nextWave);
     gravitybox.render();
     wavesUnleashed += 1;
--- gravitybox.js
+++ gravitybox.js
@@ -3,8 +3,12 @@ function createGravityBox(opts) {
       h = opts.height,
       r = opts.r ? opts.r : 16;
 
+  if (!opts.rectRecoilFactor) {
+    opts.rectRecoilFactor = 2.0;
+  }
+
   var force = d3.layout.force()
-      .friction(0.4)
+      .friction(3.0)
       .size([w, h]);
 
   var allNodeData = [];
@@ -32,6 +36,10 @@ function createGravityBox(opts) {
     var newNodes = allNodes.enter().append(opts.nodeElementName)
       .classed(opts.nodeClass, true);
 
+    if (opts.processSelection) {
+      newNodes.each(opts.processSelection);
+    } 
+
     newNodes.each(function updateSelection(d) {
       var node = d3.select(this);
       node.attr(d.attrs);
@@ -49,7 +57,11 @@ function createGravityBox(opts) {
         i = 0,
         n = allNodeData.length;
 
-      while (++i < n) q.visit(collide(allNodeData[i]));
+      var collisionFunction = collide;
+      if (opts.useRectForCollision) {
+        collisionFunction = createCollideRectFunction;
+      }
+      while (++i < n) q.visit(collisionFunction(allNodeData[i]));
 
       allNodes
         .attr(opts.xAttr, updateX)
@@ -68,6 +80,7 @@ function createGravityBox(opts) {
             y = node.y - quad.point.y,
             l = Math.sqrt(x * x + y * y),
             r = node.radius + quad.point.radius;
+
           if (!node.fixed && l < r) {
             // The node and the node described by the quad are too close.
             // Push them away from each other.
@@ -87,30 +100,100 @@ function createGravityBox(opts) {
       }
     }
 
+    function createCollideRectFunction(node) {
+      var aLeft = node.x;
+      var aRight = node.x + node.attrs.width;
+      var aTop = node.y;
+      var aBottom = node.y + node.attrs.height;
+      var aCx = aLeft + node.attrs.width/2;
+      var aCy = aTop + node.attrs.height/2;
+
+      return function collideRect(quad, x1, y1, x2, y2) {
+        if (!node.fixed && quad.point && (quad.point !== node)) {
+          var bLeft = quad.point.x;
+          var bRight = quad.point.x + quad.point.attrs.width;
+          var bTop = quad.point.y;
+          var bBottom = quad.point.y + quad.point.attrs.height;
+
+          var missed = aDoesNotCollideWithB(aTop, aBottom, aLeft, aRight,
+            bTop, bBottom, bLeft, bRight);
+
+          if (!missed) {
+            var bCx = bLeft + quad.point.attrs.width/2;
+            var bCy = bTop + quad.point.attrs.height/2;
+            var dx = (bCx - aCx) * opts.rectRecoilFactor;
+            var dy = (bCy - aCy) * opts.rectRecoilFactor;
+
+            node.x -= dx/2;
+            node.y -= dy/2;
+            quad.point.x += dx/2;
+            quad.point.y += dy/2;
+          }
+        }
+
+        // Stop searching the quad's children if no part of the node's rect 
+        // collides with the quad (expanded by maxRectWidth).
+        return aDoesNotCollideWithB(
+          aTop, aBottom, aLeft, aRight,
+          y1 - opts.maxRectHeight, 
+          y2 + opts.maxRectHeight, 
+          x1 - opts.maxRectWidth, 
+          x2 + opts.maxRectWidth
+        );
+      }
+    }
+
+    function aDoesNotCollideWithB(aTop, aBottom, aLeft, aRight,
+      bTop, bBottom, bLeft, bRight) {
+      
+      return aTop > bBottom || aBottom < bTop || 
+        aLeft > bRight || aRight < bLeft;
+    }
+
   }
 
   // The magnet nodeData do not move. 
   // It stays under the bottom of the bounding box.
 
   function magnetNodeForNode(node) {
-    return {
+    var magnet = {
       name: 'magnet',
       fixed: true,
-      x: node.x,
-      y: h + r + 20
     };
+    if (opts.useRectForCollision) {
+      magnet.x = node.x;
+      magnet.y = h + 20;
+      magnet.attrs = {
+        width: node.attrs.width,
+        height: 20,
+      }
+    }
+    else {
+      magnet.x = node.x;
+      magnet.y = h + r + 20
+    }
+    return magnet;
   }
 
   function updateX(d) {
     if (!d.fixed) {
-      d.x = Math.max(r, Math.min(w - r, d.x));
+      if (opts.useRectForCollision) {
+        d.x = Math.max(0, Math.min(w - d.attrs.width, d.x));
+      }
+      else {
+        d.x = Math.max(0, Math.min(w - r, d.x));
+      }
     }
     return d.x;
   }
 
   function updateY(d) {
     if (!d.fixed) {
-      d.y = Math.max(r, Math.min(h - r, d.y));
+      if (opts.useRectForCollision) {
+      }
+      else {
+        d.y = Math.max(0, Math.min(h - d.attrs.height, d.y));
+      }
     }
     return d.y;
   }
